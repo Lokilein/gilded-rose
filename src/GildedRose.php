7ca6eec117bde76dc553154d94e82126b0a7961b
Svenja Behr <svenja.behr@stud.th-luebeck.de>
2019-09-19 20:07:05 +0200

    Changed code format

--- src/GildedRose.php
+++ src/GildedRose.php
@@ -1,32 +1,50 @@
 <?php
 namespace App;
 
-final class GildedRose {
+final class GildedRose
+{
     private $items = [];
-    public function __construct($items) {
+
+    public function __construct($items)
+    {
         $this->items = $items;
     }
-    public function updateQuality() {
+
+    public function updateQuality()
+    {
         foreach ($this->items as $item) {
-            if ($item->name != 'Aged Brie' and $item->name != 'Backstage passes to a TAFKAL80ETC concert') {
-                if ($item->quality > 0) {
-                    if ($item->name != 'Sulfuras, Hand of Ragnaros') {
+            if ($item->name != 'Aged Brie' and $item->name != 'Backstage passes to a TAFKAL80ETC concert')
+            {
+                if ($item->quality > 0)
+                {
+                    if ($item->name != 'Sulfuras, Hand of Ragnaros')
+                    {
                         $item->quality = $item->quality - 1;
-                    } else {
+                    }
+                    else
+                    {
                         $item->quality = 80;
                     }
                 }
-            } else {
-                if ($item->quality < 50) {
+            }
+            else
+            {
+                if ($item->quality < 50)
+                {
                     $item->quality = $item->quality + 1;
-                    if ($item->name == 'Backstage passes to a TAFKAL80ETC concert') {
-                        if ($item->sell_in < 11) {
-                            if ($item->quality < 50) {
+                    if ($item->name == 'Backstage passes to a TAFKAL80ETC concert')
+                    {
+                        if ($item->sell_in < 11)
+                        {
+                            if ($item->quality < 50)
+                            {
                                 $item->quality = $item->quality + 1;
                             }
                         }
-                        if ($item->sell_in < 6) {
-                            if ($item->quality < 50) {
+                        if ($item->sell_in < 6)
+                        {
+                            if ($item->quality < 50)
+                            {
                                 $item->quality = $item->quality + 1;
                             }
                         }
@@ -34,23 +52,34 @@ final class GildedRose {
                 }
             }
 
-            if ($item->name != 'Sulfuras, Hand of Ragnaros') {
+            if ($item->name != 'Sulfuras, Hand of Ragnaros')
+            {
                 $item->sell_in = $item->sell_in - 1;
             }
 
-            if ($item->sell_in < 0) {
-                if ($item->name != 'Aged Brie') {
-                    if ($item->name != 'Backstage passes to a TAFKAL80ETC concert') {
-                        if ($item->quality > 0) {
-                            if ($item->name != 'Sulfuras, Hand of Ragnaros') {
+            if ($item->sell_in < 0)
+            {
+                if ($item->name != 'Aged Brie')
+                {
+                    if ($item->name != 'Backstage passes to a TAFKAL80ETC concert')
+                    {
+                        if ($item->quality > 0)
+                        {
+                            if ($item->name != 'Sulfuras, Hand of Ragnaros')
+                            {
                                 $item->quality = $item->quality - 1;
                             }
                         }
-                    } else {
+                    }
+                    else
+                    {
                         $item->quality = $item->quality - $item->quality;
                     }
-                } else {
-                    if ($item->quality < 50) {
+                }
+                else
+                {
+                    if ($item->quality < 50)
+                    {
                         $item->quality = $item->quality + 1;
                     }
                 }
